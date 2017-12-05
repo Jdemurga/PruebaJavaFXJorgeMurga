a9038325bb993ad99f90b53b2f5649cd86f1c081
jorge <jdemurga95@gmail.com>
2017-12-05 17:53:35 +0100

    segundo

--- src/control/ControlFX.java
+++ src/control/ControlFX.java
@@ -2,7 +2,11 @@ package control;
 
 import java.io.IOException;
 
+import com.sun.glass.events.MouseEvent;
+
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
@@ -14,10 +18,14 @@ public class ControlFX {
 	Button started;
 	
 	@FXML
-	public void apertura() throws IOException {
+	public void openStage() throws IOException {
 		Stage thisStage = (Stage) started.getScene().getWindow();
 		thisStage.close();
 		menu.abrir(new Stage());
 	}
-
+	@FXML
+	public void closeStage() {
+		System.exit(0);
+	}
+	
 }
